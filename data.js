d6b704b904513eba1363500782b7a3abc42527ba
telugupatiapoorva-cell <telugupatiapoorva@gmail.com>
2026-02-14 15:53:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,63 +1,87 @@
 const data = {
 
-general:{
-English:[
-{name:"Part A - English by Nisa", link:"https://youtube.com/playlist?list=PLUJXB7al70IB4mDWQIPyriRRylnwjPU2D"},
-{name:"Part B - English by Nisa", link:"https://youtube.com/playlist?list=PLUJXB7al70IBdkE4oiI4d40mycUq4JnQ8"},
-{name:"Whole - Sara’s Learning Point", link:"https://youtube.com/playlist?list=PLjNdhMBtsxpwAyVG7o59hqSCUYKKHvz-Y"}
-],
-Telugu:[
-{name:"Dandu Venkatramulu", link:"https://youtube.com/playlist?list=PLyjVm5GfnmY6UUa5iLn-GQNPkW7y1CeMh"},
-{name:"Matrubhasha", link:"https://youtube.com/playlist?list=PL7noKDJp4ZRKVkWtOlT6edyqABvC-zjNI"}
-],
-Hindi:[
-{name:"Disha Hindi Classes", link:"https://youtube.com/playlist?list=PLsTBUOfP6EhdnT2rEau2eaprGMdw6BZra"}
-],
-Sanskrit:[
-{name:"Dandu Venkatramulu", link:"https://youtube.com/playlist?list=PLyjVm5GfnmY7ynZnZ0neH3LcGAi1oBzHh"}
-]
+general: {
+  "English": [
+    { name: "Part A - English by Nisa", link: "https://youtube.com/playlist?list=PLUJXB7al70IB4mDWQIPyriRRylnwjPU2D" },
+    { name: "Part B - English by Nisa", link: "https://youtube.com/playlist?list=PLUJXB7al70IBdkE4oiI4d40mycUq4JnQ8" },
+    { name: "Whole - Sara’s Learning Point", link: "https://youtube.com/playlist?list=PLjNdhMBtsxpwAyVG7o59hqSCUYKKHvz-Y" },
+    { name: "Whole - Dandu Venkatramulu", link: "https://youtube.com/playlist?list=PLyjVm5GfnmY6rkPqkpH7psUaOBb44ShwS" },
+    { name: "Whole - Educare", link: "https://youtube.com/playlist?list=PLbBCm7TkLCKJtzjewd_X4NrbTBd4eCy_M" },
+    { name: "Whole - PCV English", link: "https://youtube.com/playlist?list=PLldWy7gu4R757w2-mkgFz7HDzDVhp-GkT" }
+  ],
+
+  "Telugu": [
+    { name: "Whole - Dandu Venkatramulu", link: "https://youtube.com/playlist?list=PLyjVm5GfnmY6UUa5iLn-GQNPkW7y1CeMh" },
+    { name: "Whole - Matrubhasha", link: "https://youtube.com/playlist?list=PL7noKDJp4ZRKVkWtOlT6edyqABvC-zjNI" }
+  ],
+
+  "Hindi": [
+    { name: "Whole - Disha Hindi Classes", link: "https://youtube.com/playlist?list=PLsTBUOfP6EhdnT2rEau2eaprGMdw6BZra" }
+  ],
+
+  "Sanskrit": [
+    { name: "Whole - Dandu Venkatramulu", link: "https://youtube.com/playlist?list=PLyjVm5GfnmY7ynZnZ0neH3LcGAi1oBzHh" },
+    { name: "Whole - My Teaching Talkies", link: "https://youtube.com/playlist?list=PLc1Q__0qOGAoAuv-LLipyNqpatmyE_7KJ" },
+    { name: "Whole - Dr. Pusapati Ravikantha Reddy", link: "https://youtube.com/playlist?list=PLiOV0508aWZti2c9x-V-WxJCmGLW2EN3e" }
+  ],
+
+  "Cost Control and Management Accounting": [
+    { name: "Part 1 - Hasham Ali Khan", link: "https://youtube.com/playlist?list=PLWJDzVuPkXAllmGP19jfhneNUdgOy1g2v" },
+    { name: "Part 2 - Hasham Ali Khan", link: "https://youtube.com/playlist?list=PLWJDzVuPkXAlg4vnE3ZhB7UVp9JWl6Mog" },
+    { name: "Whole - Devika’s Commerce", link: "https://youtube.com/playlist?list=PLLhSIFfDZcUVaBuGIMvQFx1OVGzfhbPeC" }
+  ],
+
+  "Theory and Practice of GST": [
+    { name: "Whole - Hasham Ali Khan", link: "https://youtube.com/playlist?list=PLWJDzVuPkXAkNpdAIqufc2JxDoxjENrsj" }
+  ],
+
+  "Accounting Standards": [
+    { name: "Whole - Devika’s Commerce", link: "https://youtube.com/playlist?list=PLLhSIFfDZcUXO81uWFy_F6O4ZX0JfTYkV" },
+    { name: "Whole - Hasham Ali Khan", link: "https://youtube.com/playlist?list=PLWJDzVuPkXAmAI6CfaLfYpQVvQFhXzxC6" }
+  ],
+
+  "Research Methodology & Project": [
+    { name: "Whole - Devika’s Commerce", link: "https://youtube.com/playlist?list=PLLhSIFfDZcUWRlgiXMkd1rNeLSz1You4O" },
+    { name: "Whole - Lakshya Commerce", link: "https://youtube.com/playlist?list=PLjMjkIR4-dvIAvboLjtq6sSi_1_wk2VgD" },
+    { name: "Whole - Pooja Singh", link: "https://youtube.com/playlist?list=PLk3poRqYftlhDGto5oSxeNeFWHzajbVuA" }
+  ]
+},
+
+honors: {
+  "English": [],
+
+  "International Finance": [
+    { name: "Whole - Hasham Ali Khan", link: "https://youtube.com/playlist?list=PLWJDzVuPkXAnNpRjN6InK65R7ndvpYZUf" }
+  ],
+
+  "Cost Control and Management Accounting": [],
+  "Theory and Practice of GST": [],
+  "Accounting Standards": [],
+  "Research Methodology & Project": []
+},
+
+computer: {
+  "English": [],
+  "Telugu": [],
+  "Hindi": [],
+  "Sanskrit": [],
+  "Cost Control and Management Accounting": [],
+  "Theory and Practice of GST": [],
+  "Cybersecurity": [
+    { name: "Whole - Cybersecurity Playlist", link: "https://youtube.com/playlist?list=PLtfg0YD2YYUQMZp4bRkeZI0JXWHGqh8vo" }
+  ],
+  "Research Methodology & Project": []
+},
+
+analytics: {
+  "English": [],
+  "Telugu": [],
+  "Hindi": [],
+  "Sanskrit": [],
+  "Cost Control and Management Accounting": [],
+  "Theory and Practice of GST": [],
+  "Business Application of Emerging Technologies": [],
+  "Research Methodology & Project": []
 }
 
 };
-
-function loadSubjects(){
-const course=document.getElementById("courseSelect").value;
-const subjectSelect=document.getElementById("subjectSelect");
-subjectSelect.innerHTML="<option value=''>Select Subject</option>";
-document.getElementById("playlistContainer").innerHTML="";
-
-if(course && data[course]){
-Object.keys(data[course]).forEach(subject=>{
-let option=document.createElement("option");
-option.value=subject;
-option.textContent=subject;
-subjectSelect.appendChild(option);
-});
-}
-}
-
-function loadPlaylists(){
-const course=document.getElementById("courseSelect").value;
-const subject=document.getElementById("subjectSelect").value;
-const container=document.getElementById("playlistContainer");
-container.innerHTML="";
-
-if(course && subject){
-data[course][subject].forEach(item=>{
-let div=document.createElement("div");
-div.className="playlist-item";
-div.textContent=item.name;
-div.onclick=()=>window.open(item.link,"_blank");
-container.appendChild(div);
-});
-}
-}
-
-function searchPlaylists(){
-let input=document.getElementById("searchBox").value.toLowerCase();
-let items=document.getElementsByClassName("playlist-item");
-for(let i=0;i<items.length;i++){
-let txt=items[i].textContent.toLowerCase();
-items[i].style.display=txt.includes(input) ? "block" : "none";
-}
-}
